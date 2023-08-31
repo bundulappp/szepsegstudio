17823bdafe02dcff34620c9cdc2dd25d03a3abee
Patrik Bundula <patrik.bundula@gammaorg.hu>
2023-08-31 12:43:14 +0200

    put everything together

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-const collapsibles = document.querySelectorAll(".collapsible");
+const collapsibles = document.querySelectorAll('.collapsible');
 collapsibles.forEach((item) =>
-  item.addEventListener("click", function () {
-    this.classList.toggle("collapsible--expanded");
+  item.addEventListener('click', function () {
+    this.classList.toggle('collapsible--expanded');
   })
 );
+
+function initMap() {
+  var storeLocation = { lat: -25.363, lng: 131.044 };
+  var map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 4,
+    center: storeLocation,
+  });
+  var marker = new google.maps.Marker({
+    position: storeLocation,
+    map: map,
+  });
+}
